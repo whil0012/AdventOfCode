f99c8c67d09361ae8b1f88484140db22492c7346
whil0012 <whil0012@gmail.com>
2017-01-04 13:15:21 -0500

    Day 10 - Add description and empty unit

--- AdventOfCodeDay10.py
+++ AdventOfCodeDay10.py
@@ -2,5 +2,45 @@ import unittest
 
 
 class TestAdventOfCodeDay10(unittest.TestCase):
-    def testEmptyTest_WhenRun_Fails(self):
-        self.assertEqual(False, True, "empty test")
+    def testLookSay_When1_Returns11(self):
+        next_value = look_say("1")
+        self.assertEqual(next_value, "11")
+
+    def testLookSay_When11_Returns21(self):
+        next_value = look_say("11")
+        self.assertEqual(next_value, "21")
+
+    def testLookSay_When111221_Returns312211(self):
+        next_value = look_say("111221")
+        self.assertEqual(next_value, "312211")
+
+
+def look_say(value):
+    last_digit = value[0]
+    current_digit_count = 0
+    result = ""
+    for digit in value:
+        if (digit != last_digit):
+            result = result + str(current_digit_count) + last_digit
+            current_digit_count = 1
+        else:
+            current_digit_count += 1
+        last_digit = digit
+    result = result + str(current_digit_count) + last_digit
+    return result
+
+
+def main():
+    next_value = "3113322113"
+    for _i in range(40):
+        next_value = look_say(next_value)
+    print("final value (40): ", next_value)
+    print("length: ", len(next_value))
+    for _i in range(10):
+        next_value = look_say(next_value)
+    print("final value (50): ", next_value)
+    print("length: ", len(next_value))
+
+
+if __name__ == '__main__':
+    main()
