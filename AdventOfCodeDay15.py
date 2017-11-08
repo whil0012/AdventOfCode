d60d5e0a9216f4a823f8a77d9dba55b00a10e219
whil0012 <whil0012@gmail.com>
2017-11-08 13:22:23 -0500

    Day 15 - Part 1 solution and part 2 description

--- AdventOfCodeDay15.py
+++ AdventOfCodeDay15.py
@@ -1,8 +1,14 @@
 import unittest
-import itertools
+import NamedPropertyItem
 
 
 class TestAdventOfCodeDay15(unittest.TestCase):
+    def get_butterscotch_named_property_item(self):
+        return NamedPropertyItem.named_property_item(name="Butterscotch", capacity=-1, durability=-2, flavor=6, texture=3, calories=8)
+
+    def get_cinnamon_named_property_item(self):
+        return NamedPropertyItem.named_property_item(name="Cinnamon", capacity=2, durability=3, flavor=-2, texture=-1, calories=3)
+
     def test_get_all_combinations_for_100_WhenInvoked_ReturnsOnlyCombinationsThatSumTo100(self):
         actual = get_all_combinations_for_100()
         for combination in actual:
@@ -17,6 +23,13 @@ class TestAdventOfCodeDay15(unittest.TestCase):
                 self.assertTrue(number >= 1, "number: " + str(number) + "; combination: " + str(combination))
                 self.assertTrue(number <= 97, "number: " + str(number) + "; combination: " + str(combination))
 
+    def test_get_score_WhenButterscotchAndCinnamon_ReturnsCorrectScore(self):
+        butterscotch = self.get_butterscotch_named_property_item()
+        cinnamon = self.get_cinnamon_named_property_item()
+        recipe = [(butterscotch, 44), (cinnamon, 56)]
+        actual = get_recipe_score(recipe)
+        self.assertEqual(actual, 62842880)
+
 
 
 def get_all_combinations_for_100():
@@ -24,3 +37,52 @@ def get_all_combinations_for_100():
         for second_number in range(1, 99 - first_number):
             for third_number in range(1, 100 - (first_number + second_number)):
                 yield [first_number, second_number, third_number, 100 - (first_number + second_number + third_number)]
+
+
+def get_capacity(recipe_item):
+    return recipe_item[0].capacity * recipe_item[1]
+
+
+def get_durability(recipe_item):
+    return recipe_item[0].durability * recipe_item[1]
+
+
+def get_flavor(recipe_item):
+    return recipe_item[0].flavor * recipe_item[1]
+
+
+def get_texture(recipe_item):
+    return recipe_item[0].texture * recipe_item[1]
+
+
+def get_recipe_score(recipe):
+    scores = [0, 0, 0, 0]
+    for recipe_item in recipe:
+        scores[0] += get_capacity(recipe_item)
+        scores[1] += get_durability(recipe_item)
+        scores[2] += get_flavor(recipe_item)
+        scores[3] += get_texture(recipe_item)
+    if any(score <= 0 for score in scores):
+        return 0
+    return scores[0] * scores[1] * scores[2] * scores[3]
+
+
+sprinkles = NamedPropertyItem.named_property_item(name="Sprinkles", capacity=5, durability=-1, flavor=0, texture=0, calories=5)
+peanut_butter = NamedPropertyItem.named_property_item(name="PeanutButter", capacity=-1, durability=3, flavor=0, texture=0, calories=1)
+frosting = NamedPropertyItem.named_property_item(name="Frosting", capacity=0, durability=-1, flavor=4, texture=0, calories=6)
+sugar = NamedPropertyItem.named_property_item(name="Sugar", capacity=-1, durability=0, flavor=0, texture=2, calories=8)
+
+
+def main():
+    four_ingredient_amount_combinations = get_all_combinations_for_100()
+    max_score = 0
+    for combination in four_ingredient_amount_combinations:
+        recipe = [(sprinkles, combination[0]), (peanut_butter, combination[1]), (frosting, combination[2]), (sugar, combination[3])]
+        score = get_recipe_score(recipe)
+        if score > max_score:
+            max_score = score
+    print("max score: ", max_score)
+
+
+if __name__ == "__main__":
+    main()
