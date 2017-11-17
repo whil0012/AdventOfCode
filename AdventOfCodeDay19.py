de38053811529b727e8cd7006c2efb9b253537cd
whil0012 <whil0012@gmail.com>
2017-11-17 14:06:10 -0500

    Day 19 - First unit test

--- AdventOfCodeDay19.py
+++ AdventOfCodeDay19.py
@@ -3,5 +3,12 @@ import os
 
 
 class TestAdventOfCodeDay19(unittest.TestCase):
-    def test_when_empty_fails(self):
-        self.assertFalse(True, "empty test")
+    def test_get_distinct_molecules_WhenThreeTransitionsAndHOHStart_Returns4DistinctMolecules(self):
+        transitions = {'H' : ['HO', 'OH'], 'O': ['HH']}
+        actual = get_distinct_molecules(transitions, 'HOH')
+        expected = set(['HOOH', 'HOHO', 'OHOH', 'HHHH'])
+        self.assertSetEqual(actual, expected)
+
+
+def get_distinct_molecules(transitions, param):
+    return set()
