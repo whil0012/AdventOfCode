df14c6bb07a63994a258464d53e642b3d2047f67
whil0012 <whil0012@users.noreply.github.com>
2017-11-18 12:00:42 -0500

    Day 19 - Notes for solution using string find and slicing

--- AdventOfCodeDay19.py
+++ AdventOfCodeDay19.py
@@ -10,5 +10,11 @@ class TestAdventOfCodeDay19(unittest.TestCase):
         self.assertSetEqual(actual, expected)
 
 
-def get_distinct_molecules(transitions, param):
-    return set()
+def get_distinct_molecules(transitions, starting_molecule):
+    result = set()
+    # occurrence = starting_molecule.find(item_to_replace, 0)
+    # new_molecule = starting_molecule[:occurrence] + item_replacement + test_string[occurence + len(item_to_replace):]
+    # occurrence = starting_molecule.find(item_to_replace, occurrence + 1)
+    # ... until occurrence == -1 || while occurrence >= 0, etc
+    # is there a do until? or do { } while?
+    return result
