59e4b5aec312faf568be8d3aeead9f9d03414a64
whil0012 <whil0012@gmail.com>
2017-11-19 13:03:39 -0500

    Day 19 - Part 2 - Bad solution

--- AdventOfCodeDay19.py
+++ AdventOfCodeDay19.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import NamedPropertyItem
 
 
 class TestAdventOfCodeDay19(unittest.TestCase):
@@ -15,9 +16,66 @@ class TestAdventOfCodeDay19(unittest.TestCase):
         expected = {'Al': ['ThF', 'ThRnFAr'], 'B': ['BCa']}
         self.assertDictEqual(actual, expected)
 
+    def test_get_transitions_count_WhenHOH_Returns3(self):
+        transitions = [NamedPropertyItem.named_property_item(item = 'H', replacement = 'HO'),
+                    NamedPropertyItem.named_property_item(item='H', replacement='OH'),
+                    NamedPropertyItem.named_property_item(item='O', replacement='HH'),
+                    NamedPropertyItem.named_property_item(item='e', replacement='H'),
+                    NamedPropertyItem.named_property_item(item='e', replacement='O')]
+        actual = get_transitions_count(transitions, 'HOH')
+        self.assertEqual(actual, 3)
+
+    def test_get_transitions_count_WhenHOHOHO_Returns6(self):
+        transitions = [NamedPropertyItem.named_property_item(item = 'H', replacement = 'HO'),
+                    NamedPropertyItem.named_property_item(item='H', replacement='OH'),
+                    NamedPropertyItem.named_property_item(item='O', replacement='HH'),
+                    NamedPropertyItem.named_property_item(item='e', replacement='H'),
+                    NamedPropertyItem.named_property_item(item='e', replacement='O')]
+        actual = get_transitions_count(transitions, 'HOHOHO')
+        self.assertEqual(actual, 6)
+
 
 line_segment_index_item_to_replace = 0
 line_segment_index_replacement = 2
+
+
+def get_transitions_count(transitions, desired_molecule):
+    non_e_transitions = get_non_e_transitions(transitions)
+    count = make_reverse_transitions(non_e_transitions, desired_molecule)
+    return count + 1
+
+
+def make_reverse_transitions(transitions, desired_molecule):
+    total_count = 0
+    current_molecule = desired_molecule
+    transitions_sorted = sorted(transitions, key=lambda x: len(x.replacement), reverse=True)
+    attempt_more_transitions = True
+    while attempt_more_transitions:
+        transitions_loop_count = 0
+        for transition in transitions_sorted:
+            transitions_result = make_reverse_transitions_for_item(transition, current_molecule)
+            transitions_loop_count += transitions_result.count
+            current_molecule = transitions_result.molecule
+        total_count += transitions_loop_count
+        attempt_more_transitions = transitions_loop_count > 0
+    return total_count
+
+
+def make_reverse_transitions_for_item(transition, molecule):
+    return make_reverse_transitions_for_item_replacement(transition.item, transition.replacement, molecule)
+
+
+def make_reverse_transitions_for_item_replacement(item, replacement, molecule):
+    transitions_count = 0
+    current_molecule = molecule
+    while replacement in current_molecule:
+        current_molecule = current_molecule.replace(replacement, item, 1)
+        transitions_count += 1
+    return NamedPropertyItem.named_property_item(molecule = current_molecule, count = transitions_count)
+
+
+def get_non_e_transitions(transitions):
+    return [x for x in transitions if x.item != 'e']
 
 
 def add_transition_from_line(line, transitions):
@@ -38,6 +96,17 @@ def get_transitions_from_lines(lines):
     result = {}
     add_transitions_from_lines(lines, result)
     return result
+
+
+def get_transition_named_property_item(line):
+    line_segments = line.strip().split(' ')
+    item_to_replace = line_segments[line_segment_index_item_to_replace]
+    replacement = line_segments[line_segment_index_replacement]
+    return NamedPropertyItem.named_property_item(item = item_to_replace, replacement = replacement)
+
+
+def get_exploded_transitions_list(lines):
+    return [get_transition_named_property_item(x) for x in lines]
 
 
 def add_distinct_molecules_for_replacement_item(starting_molecule, item_to_replace, replacement_item,
@@ -73,8 +142,12 @@ def main():
     file_path = os.path.join("input", "day_19_transitions.txt")
     with open(file_path, "r") as input_file:
         transitions = get_transitions_from_lines(input_file)
+        input_file.seek(0)
+        transitions_list = get_exploded_transitions_list(input_file)
     distinct_molecules = get_distinct_molecules(transitions, formula)
     print("number of distinct molecules: ", len(distinct_molecules))
+    transitions_count = get_transitions_count(transitions_list, formula)
+    print("number of transitions: ", transitions_count)
 
 
 if __name__ == "__main__":
